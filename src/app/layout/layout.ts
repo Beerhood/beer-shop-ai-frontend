92b04a5fd9350f741ea0677ec9d48219c6c97a55
Kurenkov Maxim <159890193+KurenkovMaxgit@users.noreply.github.com>
2025-11-05 05:24:09 +0200

    feat: add menu bar in layout

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
+import { BadgeModule } from 'primeng/badge';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet],
+  imports: [MenubarModule, BadgeModule, InputTextModule, CommonModule, ButtonModule, RouterOutlet],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
-export class Layout {}
+export class Layout implements OnInit {
+  items: MenuItem[] | undefined;
+  icon = signal<string>('pi pi-sun');
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home',
+        routerLink: '/home',
+      },
+      {
+        label: 'Menu',
+        icon: 'pi pi-clipboard',
+        routerLink: '/menu',
+      },
+    ];
+    this.applyTheme();
+  }
+
+  toggleDarkMode() {
+    const element = document.querySelector('html');
+    const isDarkMode = element?.classList.contains('dark-theme');
+    element?.classList.toggle('dark-theme', !isDarkMode);
+    localStorage.setItem('theme', isDarkMode ? 'light' : 'dark');
+
+    if (this.icon() === 'pi pi-sun') {
+      this.icon.set('pi pi-moon');
+    } else {
+      this.icon.set('pi pi-sun');
+    }
+  }
+
+  private applyTheme() {
+    const savedTheme = localStorage.getItem('theme') || 'light';
+    const element = document.querySelector('html');
+    const isDarkMode = savedTheme === 'dark';
+
+    element?.classList.toggle('dark-theme', isDarkMode);
+
+    this.icon.set(isDarkMode ? 'pi pi-moon' : 'pi pi-sun');
+  }
+}
