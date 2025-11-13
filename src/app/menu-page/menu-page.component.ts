f522f98f8b5a98dc5ed0cf53b8ec701c18c64f54
Kurenkov Maxim <159890193+KurenkovMaxgit@users.noreply.github.com>
2025-11-13 22:43:11 +0200

    feat: update menu page

--- src/app/menu-page/menu-page.component.ts
+++ src/app/menu-page/menu-page.component.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Card } from '../components/card/card';
+import * as MOCK from 'src/app/models/products.mock.json';
+import { IProductsApiResponse } from '../models';
+import { ProductService } from '../services/product.service';
+import { PaginatorModule, PaginatorState } from 'primeng/paginator';
 
 @Component({
   selector: 'app-menu-page.component',
-  imports: [],
-  template: `<p>menu-page works!</p>`,
+  imports: [Card, PaginatorModule],
+  templateUrl: './menu-page.component.html',
+  styleUrl: './menu-page.component.scss',
 })
-export class MenuPage {}
+export class MenuPage implements OnInit {
+  first: number = 0;
+
+  rows: number = 3;
+  products = <IProductsApiResponse>MOCK;//MOCK for now, rewrite using signal() in future
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  onPageChange(event: PaginatorState) {
+    this.first = event.first ?? 0;
+    this.rows = event.rows ?? 10;
+  }
+
+  getProducts(): void {
+    this.productService.getList(); //TODO: add pagination & filter field overloads
+  }
+}
