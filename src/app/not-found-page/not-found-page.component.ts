3d05dac138cc10cd6ae2f545214789912dd4b096
Kurenkov Maxim <159890193+KurenkovMaxgit@users.noreply.github.com>
2025-11-15 01:41:38 +0200

    feat: update not found page

--- src/app/not-found-page/not-found-page.component.ts
+++ src/app/not-found-page/not-found-page.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-not-found-page',
   imports: [],
-  template: ` <p>not-found-page works!</p> `,
+  template: ` <div class="flex justify-center items-center min-h-screen flex-col">
+    <img src="./assets/disintegrated-chipushila.gif" alt="disintegrated chepushila" class="rounded-2xl opacity-85">
+    <h1 class="font-extrabold text-7xl">Page not found</h1>
+  </div> `,
 })
 export class NotFoundPage {}
