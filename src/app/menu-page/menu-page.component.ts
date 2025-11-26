d62160f13cc74af0cc124234d2aae292a768591c
Kurenkov Maxim <159890193+KurenkovMaxgit@users.noreply.github.com>
2025-11-26 12:19:18 +0200

    feat: update menu page component

--- src/app/menu-page/menu-page.component.ts
+++ src/app/menu-page/menu-page.component.ts
@@ -1,26 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { Card } from '../components/card/card';
 import * as MOCK from 'src/app/models/products.mock.json';
-import { IProductsApiResponse } from '../models';
+import { IProduct, IProductsApiResponse } from '../models';
 import { ProductService } from '../services/product.service';
 import { PaginatorModule, PaginatorState } from 'primeng/paginator';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ProductModal } from '../components/product-modal/product-modal';
+import { SnackbarService } from '../services/snackbar.service';
 
 @Component({
   selector: 'app-menu-page.component',
-  imports: [Card, PaginatorModule],
+  imports: [Card, PaginatorModule, ProductModal],
   templateUrl: './menu-page.component.html',
   styleUrl: './menu-page.component.scss',
 })
 export class MenuPage implements OnInit {
   first: number = 0;
-
   rows: number = 3;
-  products = <IProductsApiResponse>MOCK;//MOCK for now, rewrite using signal() in future
-
-  constructor(private productService: ProductService) {}
+  products = <IProductsApiResponse>MOCK; //MOCK for now, rewrite using signal() in future
+  dialogVisible = false;
+  selectedProduct: IProduct;
+  constructor(
+    private productService: ProductService,
+    private snackBar: SnackbarService,
+    private readonly router: Router,
+    private readonly route: ActivatedRoute,
+  ) {}
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.route.paramMap.subscribe((params) => {
+      const id = params.get('id');
+
+      if (id) {
+        this.openProductById(id);
+      } else {
+        this.dialogVisible = false;
+      }
+    });
+  }
+
+  openProductById(id: string) {
+    const product = this.products.items.find((p) => p._id === id);
+
+    if (!product) {
+      this.dialogVisible = false;
+      this.router.navigate(['menu']);
+      this.snackBar.showError('Product not found');
+
+      return;
+    }
+    this.selectedProduct = product;
+    this.dialogVisible = true;
   }
 
   onPageChange(event: PaginatorState) {
